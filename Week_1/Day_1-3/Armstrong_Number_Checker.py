5142169bf96966b66451d67d614cfa7ecd5c0ebf
Yogendiran-SP <jyogendiran@gmail.com>
2025-04-06 18:50:09 +0530

    Created Armstrong Number Checker

--- Week_1/Day_1-3/Armstrong_Number_Checker.py
+++ Week_1/Day_1-3/Armstrong_Number_Checker.py
@@ -1,6 +1,13 @@
 def checking(a):
     copy,res = a,0
     while(a>0):
-        res += (a%10) ** len(str(a))
-        a = a // 10
-    result = "Armstrong"
+        res += (a%10)**len(str(copy))
+        a = a//10
+    result = copy, "is an Armstrong Number" if copy==res else "is not an Armstrong Number"
+    return result
+
+if __name__=="__main__":
+    while True:
+        print("\nCHECKING WHETHER A NUMBER IS ARMSTRONG OR NOT\n_____________________________________________\n")
+        a=int(input("Enter the number: "))
+        print(*checking(a),"\n_____________________________________________\n")
