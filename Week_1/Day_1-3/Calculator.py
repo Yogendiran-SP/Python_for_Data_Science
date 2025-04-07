75ff62cfa9bf4a821f240ef974eeebb47b23723e
Yogendiran-SP <jyogendiran@gmail.com>
2025-04-07 15:08:07 +0530

    Created Calculator

--- Week_1/Day_1-3/Calculator.py
+++ Week_1/Day_1-3/Calculator.py
@@ -12,12 +12,13 @@ def opcalc(a,b,c):
 
 if __name__=="__main__":
     while True:
+        print("\nCALCULATOR\n__________\n")
         print("1. Calculate using Eval function\n2. Calculate using operators & operands")
         choice=int(input("Enter your choice (1 or 2): "))
 
         if choice==1:
             try:
-                print("Result: ",evalcalc(input("Enter the calculation: ")))
+                print("\nResult: ",evalcalc(input("Enter the calculation: ")))
             except:
                 print("Invalid input")
         elif choice==2:
@@ -25,7 +26,7 @@ if __name__=="__main__":
                 a=int(input("Enter the first operand: "))
                 b=input("Enter the operator: ")
                 c=int(input("Enter the second operand: "))
-                print("Result: ",opcalc(a,b,c))
+                print("\nResult: ",opcalc(a,b,c))
             except:
                 print("Invalid input")
         else:
